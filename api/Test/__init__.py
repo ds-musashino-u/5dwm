9924d84e04a66d906d5dab20bcb2626e4930e7fa
Milch <kawatan@gmail.com>
2021-12-06 13:26:33 +0900

    Update __init__.py

--- api/Test/__init__.py
+++ api/Test/__init__.py
@@ -1,12 +1,19 @@
 import logging
+import time
+import json
 
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    headers = {'Content-Type': 'application/json'}
 
+    if 'HTTP_ORIGIN' in req.headers:
+        headers['Access-Control-Allow-Origin'] = req.headers['HTTP_ORIGIN']
+    
     name = req.params.get('name')
+
     if not name:
         try:
             req_body = req.get_json()
@@ -16,9 +23,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(json.dumps({
+                'text': name,
+                'timestamp': int(time.time())
+            }),
+            status_code=200,
+            headers=headers,
+            charset='utf-8')
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
         )
