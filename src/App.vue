5b307ccdffa2a3de3b249d0640bf40451405c2de
Milch <kawatan@gmail.com>
2021-12-17 18:17:48 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  data() {
+    return {
+      page: null
+    }
+  }
+}
 </script>
 
 <template>
