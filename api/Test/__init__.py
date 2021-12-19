d3952658a29162e9364a7893a2993b85cd21c79b
Milch <kawatan@gmail.com>
2021-12-19 13:00:03 +0900

    Update __init__.py

--- api/Test/__init__.py
+++ api/Test/__init__.py
@@ -27,7 +27,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         try:
             return func.HttpResponse(json.dumps({
                     'text': name,
-                    'key': os.environ["ACCESS_KEY"],
+                    'key': os.environ.get["ACCESS_KEY"],
                     'timestamp': int(time.time())
                 }),
                 status_code=200,
