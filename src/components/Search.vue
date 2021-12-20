f0252491a692583664f795a3bb89cdb1beb3a56c
Milch <kawatan@gmail.com>
2021-12-20 22:25:43 +0900

    Add Search component

---
+++ src/components/Search.vue
@@ -0,0 +1,22 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { ref } from "vue";
+
+/*const props = defineProps({
+  items: Array
+});
+const emit = defineEmits(["reveal", "select"]);
+const select = (event) => {
+  emit("select", event.target.dataset);
+};*/
+</script>
+
+<template>
+  <div id="search">
+    <span>Search</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
