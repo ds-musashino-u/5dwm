cece912c6359314ea270a8bb41f591373278362f
Milch <kawatan@gmail.com>
2022-01-05 13:52:50 +0900

    Update __init__.py

--- api/upload/__init__.py
+++ api/upload/__init__.py
@@ -1,8 +1,9 @@
 import time
+import re
 import json
-import uuid
 import logging
 import os
+from uuid import uuid4
 
 import azure.functions as func
 from azure.storage.blob import BlobServiceClient
@@ -14,25 +15,41 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         headers['Access-Control-Allow-Origin'] = req.headers['Origin']
 
     try:
-        req_body = req.get_json()
-        image = req_body.get("image")
+        pattern = "data:([\\w/\\-\\.]+);(\\w+),(.+)"
         
-        blob_service_client = BlobServiceClient.from_connection_string(os.environ.get("AZURE_STORAGE_CONNECTION_STRING"))
-        container_client = blob_service_client.get_container_client("images")
-    
-        blob = str(uuid.uuid4())
-        blob_client = container_client.get_blob_client(blob)
-        #blob_client.upload_blob(data, blob_type="BlockBlob")
-        #download_stream = blob_client.download_blob()
-        #download_stream.readall()
+        if req.headers['Content-Type'] == 'application/json':
+            data = req.get_json()            
+            match = re.match(pattern, data.get('image'))
+            path = data.get('path')
+            
+        else:
+            match = re.match(pattern, req.get_body.decode('utf-8'))
+            path = req.params.get('path')
 
-        return func.HttpResponse(json.dumps({
-                'blob': blob,
-                'timestamp': int(time.time())
-            }),
-            status_code=200,
-            headers=headers,
-            charset='utf-8')
+        if match:
+            mime_type, encoding, data = match.groups()
+        
+            if mime_type in ['image/png', 'image/jpeg'] and encoding == 'base64':
+                container_name = 'images'
+
+                if path is None:
+                    path = str(uuid4())
+        
+                blob_service_client = BlobServiceClient.from_connection_string(os.environ.get('AZURE_STORAGE_CONNECTION_STRING'))
+                container_client = blob_service_client.get_container_client(container_name)
+            
+                blob_client = container_client.get_blob_client(path)
+                blob_client.upload_blob(data, blob_type="BlockBlob")
+                #download_stream = blob_client.download_blob()
+                #download_stream.readall()
+
+                return func.HttpResponse(json.dumps({
+                        'path': path,
+                        'timestamp': int(time.time())
+                    }),
+                    status_code=200,
+                    headers=headers,
+                    charset='utf-8')
 
     except Exception as e:
         logging.error(f'{e}')
