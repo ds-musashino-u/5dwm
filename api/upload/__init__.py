9ccb5c1e7006a7fe8cfb3588421f20eb84d60f37
Milch <kawatan@gmail.com>
2022-01-11 16:36:52 +0900

    Update __init__.py

--- api/upload/__init__.py
+++ api/upload/__init__.py
@@ -3,11 +3,14 @@ import re
 import json
 import logging
 import os
+from datetime import datetime, timezone
 from uuid import uuid4
 from base64 import b64decode
+from urllib.request import urlopen, Request
 
 import azure.functions as func
 from azure.storage.blob import BlobServiceClient
+from azure.cosmos.cosmos_client import CosmosClient
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     headers = {'Content-Type': 'application/json'}
@@ -16,41 +19,50 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         headers['Access-Control-Allow-Origin'] = req.headers['Origin']
 
     try:
-        pattern = "data:([\\w/\\-\\.]+);(\\w+),(.+)"
+        if 'Authorization' in req.headers:
+            '''
+            try:
+                response = urlopen(Request(
+                    f'https://',
+                    headers={'Content-Type': 'application/json'},
+                    data=json.dumps({'idToken': req.headers['Authorization']}).encode('utf-8')))
+
+                if response.getcode() != 200:
+                    raise Exception
+
+            except Exception:
+                return func.HttpResponse(status_code=403, headers=headers)
+            '''
+            pass
         
         if req.headers['Content-Type'] == 'application/json':
             data = req.get_json()            
-            match = re.match(pattern, data.get('image'))
-            path = data.get('path')
+            match = re.match("data:([\\w/\\-\\.]+);(\\w+),(.+)", data.get('image'))
             
-        else:
-            match = re.match(pattern, req.get_body.decode('utf-8'))
-            path = req.params.get('path')
-
-        if match:
-            mime_type, encoding, data = match.groups()
-        
-            if mime_type in ['image/png', 'image/jpeg'] and encoding == 'base64':
-                container_name = 'images'
-
-                if path is None:
-                    path = str(uuid4())
-        
-                blob_service_client = BlobServiceClient.from_connection_string(os.environ.get('AZURE_STORAGE_CONNECTION_STRING'))
-                container_client = blob_service_client.get_container_client(container_name)
+            if match:
+                mime_type, encoding, data = match.groups()
             
-                blob_client = container_client.get_blob_client(path)
-                blob_client.upload_blob(b64decode(data), blob_type="BlockBlob")
-                #download_stream = blob_client.download_blob()
-                #download_stream.readall()
+                if mime_type in ['image/png', 'image/jpeg'] and encoding == 'base64':
+                    container_name = '$web'
+                    id = str(uuid4())
+                    path = f'images/{id}'
+                    
+                    blob_service_client = BlobServiceClient.from_connection_string(os.environ.get('AZURE_STORAGE_CONNECTION_STRING'))
+                    container_client = blob_service_client.get_container_client(container_name)
+                
+                    blob_client = container_client.get_blob_client(path)
+                    blob_client.upload_blob(b64decode(data), blob_type="BlockBlob")
+                    
+                    item = {'id': id, 'pk': id, 'url': '', 'type': mime_type, 'timestamp': datetime.fromtimestamp(time.time(), timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ') }
+                    
+                    client = CosmosClient.from_connection_string(os.environ.get('AZURE_COSMOS_DB_CONNECTION_STRING'))
+                    database = client.get_database_client('5DWM')
+                    container = database.get_container_client('Uploads')
+                    container.upsert_item(item)
 
-                return func.HttpResponse(json.dumps({
-                        'path': path,
-                        'timestamp': int(time.time())
-                    }),
-                    status_code=200,
-                    headers=headers,
-                    charset='utf-8')
+                    return func.HttpResponse(json.dumps(item), status_code=200, headers=headers, charset='utf-8')
+
+        return func.HttpResponse(status_code=400, headers=headers)
 
     except Exception as e:
         logging.error(f'{e}')
