719da56b338be18a84d4d87fb270f4f0d597cb5b
Milch <kawatan@gmail.com>
2022-01-11 21:12:37 +0900

    Update __init__.py

--- api/upload/__init__.py
+++ api/upload/__init__.py
@@ -21,17 +21,20 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         if 'Authorization' in req.headers:
             '''
-            try:
-                response = urlopen(Request(
-                    f'https://',
-                    headers={'Content-Type': 'application/json'},
-                    data=json.dumps({'idToken': req.headers['Authorization']}).encode('utf-8')))
+            jwt = req.headers['Authorization'].split(' ')[1].split('.') if req.headers['Authorization'].startswith('Bearer ') else req.headers['Authorization'].split('.')
 
-                if response.getcode() != 200:
-                    raise Exception
+            if json.loads(b64decode(jwt[0] + '=' * (-len(jwt[0]) % 4)))['typ'] == 'JWT' and json.loads(b64decode(jwt[1] + '=' * (-len(jwt[1]) % 4)))['iss'] == 'https://':
+                try:
+                    response = urlopen(Request(
+                        f'https://',
+                        headers={'Content-Type': 'application/json'},
+                        data=json.dumps({'idToken': req.headers['Authorization']}).encode('utf-8')))
 
-            except Exception:
-                return func.HttpResponse(status_code=403, headers=headers)
+                    if response.getcode() != 200:
+                        raise Exception
+
+                except Exception:
+                    return func.HttpResponse(status_code=403, headers=headers)
             '''
             pass
         
