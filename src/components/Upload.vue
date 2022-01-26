ce7af63606443415f561e336951d939a1f4cab91
Milch <kawatan@gmail.com>
2022-01-27 00:05:20 +0900

    Update Upload.vue

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -1,7 +1,7 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import { ref } from "vue";
+import { ref, onActivated, onDeactivated } from "vue";
 
 const props = defineProps({
   text: String,
@@ -81,7 +81,11 @@ const update = async () => {
   emit("updated");
 };
 
-update();
+onActivated(() => {
+  update();
+});
+onDeactivated(() => {
+});
 </script>
 
 <template>
