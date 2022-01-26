a557666d9f6ef9b7d2907ff27714192164725dda
Milch <kawatan@gmail.com>
2022-01-26 13:18:14 +0900

    Update Upload.vue

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -4,17 +4,85 @@
 import { ref } from "vue";
 
 const props = defineProps({
-  text: String
+  text: String,
 });
-/*const emit = defineEmits(["reveal", "select"]);
-const select = (event) => {
-  emit("select", event.target.dataset);
-};*/
+const isUploading = ref(false);
+const upload = async (event) => {
+  isUploading.value = true;
+  
+  for (const file of event.currentTarget.files) {
+    try {
+      const dataURL = await new Promise(function (resolve, reject) {
+        const reader = new FileReader();
+
+        reader.onload = () => {
+          resolve(reader.result);
+        };
+        reader.onerror = () => {
+          reject(reader.error);
+        };
+
+        if (file) {
+          reader.readAsDataURL(file);
+        }
+      });
+
+      const response = await fetch("https://www.5dworldmap.com//api/v1/upload", {
+        mode: "cors",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ image: dataURL }),
+      });
+
+      if (response.ok) {
+        console.log(await response.json());
+      } else {
+        throw new Error(response.statusText);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  isUploading.value = false;
+};
 </script>
 
 <template>
   <div id="upload">
-    <span>{{ text }}</span>
+    <div class="level">
+      <div class="level-item">
+        <span>{{ text }}</span>
+      </div>
+      <div class="level-item">
+        <span class="file button is-rounded is-size-7 has-text-weight-bold">
+          <label class="file-label">
+            <input
+              class="file-input"
+              type="file"
+              name="upload"
+              accept="image/apng, image/png, image/jpeg, image/webp"
+              style="pointer-events: none"
+              v-bind:disabled="isUploading"
+              @change="upload($event)"
+            />
+            <div class="file-cta_">
+              <transition name="fade" mode="out-in">
+                <span class="icon" v-if="isUploading" key="uploading">
+                  <i class="fas fa-spinner updating"></i>
+                </span>
+                <span class="icon" v-else key="ready">
+                  <i class="fas fa-cloud-upload-alt"></i>
+                </span>
+              </transition>
+              <span>Upload</span>
+            </div>
+          </label>
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
