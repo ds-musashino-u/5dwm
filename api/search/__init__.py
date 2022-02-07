6b859ba93f566dc0cf4319419e252036649e3300
Milch <kawatan@gmail.com>
2022-02-07 14:43:56 +0900

    Add search function

---
+++ api/search/__init__.py
@@ -0,0 +1,42 @@
+import json
+import logging
+import os
+from urllib.request import urlopen, Request
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        if 'Authorization' in req.headers:
+            '''
+            jwt = req.headers['Authorization'].split(' ')[1].split('.') if req.headers['Authorization'].startswith('Bearer ') else req.headers['Authorization'].split('.')
+
+            if json.loads(b64decode(jwt[0] + '=' * (-len(jwt[0]) % 4)))['typ'] == 'JWT' and json.loads(b64decode(jwt[1] + '=' * (-len(jwt[1]) % 4)))['iss'] == 'https://':
+                try:
+                    response = urlopen(Request(
+                        f'https://',
+                        headers={'Content-Type': 'application/json'},
+                        data=json.dumps({'idToken': req.headers['Authorization']}).encode('utf-8')))
+
+                    if response.getcode() != 200:
+                        raise Exception
+
+                except Exception:
+                    return func.HttpResponse(status_code=403, mimetype='', charset='')
+            '''
+            pass
+
+        return func.HttpResponse(json.dumps([]), status_code=200, mimetype='application/json', charset='utf-8')
+
+    except Exception as e:
+        logging.error(f'{e}')
+
+        return func.HttpResponse(json.dumps({
+            'error': {
+                'message': str(e),
+                'type': type(e).__name__}
+        }),
+            status_code=400,
+            mimetype='application/json',
+            charset='utf-8')
