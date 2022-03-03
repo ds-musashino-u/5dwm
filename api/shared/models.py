a40aa68a07ec721eca6ceea18ae488170328a57e
Milch <kawatan@gmail.com>
2022-03-03 18:06:02 +0900

    Update models.py

--- api/shared/models.py
+++ api/shared/models.py
@@ -1,7 +1,7 @@
 #from sqlalchemy import Column, Integer, String, DateTime
-#from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
-#Base = declarative_base()
+Base = declarative_base()
 
 class User():
     __tablename__ = 'users'
