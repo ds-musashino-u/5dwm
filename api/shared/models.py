a9063ba6eab1438623b1380d59f5ab504a5974e7
Milch <kawatan@gmail.com>
2022-03-03 17:59:19 +0900

    Update models.py

--- api/shared/models.py
+++ api/shared/models.py
@@ -4,6 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = "users"
-    __table_args__ = {"autoload": True}
+    __tablename__ = 'users'
+    __table_args__ = {'autoload': True}
     #id = Column(Integer, primary_key=True)
+
+    def __init__(self):
+        pass
