1c02fa6df8a3088ea2c8512025ad3d258bc76134
Milch <kawatan@gmail.com>
2022-03-03 15:57:03 +0900

    Create models.py

---
+++ api/shared/models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
