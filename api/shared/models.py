29fa5fe993ae3ce9b27d9ec30efcdf259735a2c8
Milch <kawatan@gmail.com>
2022-03-04 20:02:38 +0900

    Update models.py

--- api/shared/models.py
+++ api/shared/models.py
@@ -7,3 +7,7 @@ class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     user_cns = Column(String(20))
+    firstname = Column(String(20))
+    lastname = Column(String(20))
+    update_time = Column(DateTime)
+    email = Column(String(128))
