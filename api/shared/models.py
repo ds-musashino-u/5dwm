76fbd1f9989db83d8d62fefce2933e5c05509d62
Milch <kawatan@gmail.com>
2022-03-04 16:16:14 +0900

    Update models.py

--- api/shared/models.py
+++ api/shared/models.py
@@ -1,9 +1,12 @@
+import os
 #from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
+engine = create_engine(os.environ.get('POSTGRESQL_CONNECTION_URL'), connect_args={'sslmode':'verify-full'}, pool_recycle=60)
 Base = declarative_base()
 
-class User():
+class User(Base):
     __tablename__ = 'users'
     __table_args__ = {'autoload': True}
     #id = Column(Integer, primary_key=True)
