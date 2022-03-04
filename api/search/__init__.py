9290fa7c1e4a16e2177bbb72eb1439bf577a7fd4
Milch <kawatan@gmail.com>
2022-03-04 11:43:25 +0900

    Update __init__.py

--- api/search/__init__.py
+++ api/search/__init__.py
@@ -34,7 +34,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             '''
             pass
 
-        with psycopg2.connect(os.environ.get('POSTGRESQL_CONNECTION_URL'), sslmode='verify-ca') as connection:
+        with psycopg2.connect(os.environ.get('POSTGRESQL_CONNECTION_URL'), sslmode='require') as connection:
             with connection.cursor() as cursol:
                 return func.HttpResponse(json.dumps([]), status_code=200, mimetype='application/json', charset='utf-8')
 
