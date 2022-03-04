90602a82318b2224a3089210c58cf9d7e77875f5
Milch <kawatan@gmail.com>
2022-03-04 19:42:32 +0900

    Update models.py

--- api/shared/models.py
+++ api/shared/models.py
@@ -1,13 +1,9 @@
-import os
-#from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
-#meta = Metadata(engine)
-#meta.reflect()
-#Base = declarative_base(metadata=meta)
+Base = declarative_base()
 
-class User():
+class User(Base):
     __tablename__ = 'users'
-    __table_args__ = {'autoload': True}
-    #id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    user_cns = Column(String(20))
