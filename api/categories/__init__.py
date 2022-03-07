ea993f046781b977aa6f2a0686be31a41ae3f6b6
Milch <kawatan@gmail.com>
2022-03-07 17:16:51 +0900

    Add categories function

---
+++ api/categories/__init__.py
@@ -0,0 +1,82 @@
+import json
+import logging
+import os
+from urllib.request import urlopen, Request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from shared.models import Category
+
+import azure.functions as func
+
+
+engine = create_engine(os.environ.get('POSTGRESQL_CONNECTION_URL'), connect_args={
+                       'sslmode': 'disable'}, pool_recycle=60)
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        if 'Authorization' in req.headers:
+            '''
+            jwt = req.headers['Authorization'].split(' ')[1].split('.') if req.headers['Authorization'].startswith('Bearer ') else req.headers['Authorization'].split('.')
+
+            if json.loads(b64decode(jwt[0] + '=' * (-len(jwt[0]) % 4)))['typ'] == 'JWT' and json.loads(b64decode(jwt[1] + '=' * (-len(jwt[1]) % 4)))['iss'] == 'https://':
+                try:
+                    response = urlopen(Request(
+                        f'https://',
+                        headers={'Content-Type': 'application/json'},
+                        data=json.dumps({'idToken': req.headers['Authorization']}).encode('utf-8')))
+
+                    if response.getcode() != 200:
+                        raise Exception
+
+                except Exception:
+                    return func.HttpResponse(status_code=403, mimetype='', charset='')
+            '''
+            pass
+
+        if req.headers.get('Content-Type') == 'application/json':
+            data = req.get_json()
+            offset = data.get('offset')
+            limit = data.get('limit')
+
+        else:
+            offset = int(req.params['offset']
+                         ) if 'offset' in req.params else None
+            limit = int(req.params['limit']) if 'limit' in req.params else None
+
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        try:
+            categories = []
+            query = session.query(Category)
+
+            if limit is not None:
+                query = query.limit(limit)
+
+            if offset is not None:
+                query = query.offset(offset)
+
+            for category in query.all():
+                categories.append({
+                    'id': category.id,
+                    'name': category.name,
+                    'updated_at': category.updated_at.strftime('%Y-%m-%dT%H:%M:%SZ')
+                })
+
+            return func.HttpResponse(json.dumps(categories), status_code=200, mimetype='application/json', charset='utf-8')
+
+        finally:
+            session.close()
+
+    except Exception as e:
+        logging.error(f'{e}')
+
+        return func.HttpResponse(json.dumps({
+            'error': {
+                'message': str(e),
+                'type': type(e).__name__}
+        }),
+            status_code=400,
+            mimetype='application/json',
+            charset='utf-8')
--- api/shared/models.py
+++ api/shared/models.py
@@ -3,6 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
+class Category(Base):
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key=True)
+    name = Column('name', String(50))
+    updated_at = Column('update_time', DateTime)
+
+
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
