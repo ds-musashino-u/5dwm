2188e8e9df49e388ee85706ca6554921f1640455
Milch <kawatan@gmail.com>
2022-03-07 17:21:07 +0900

    Update endpoints.mjs

--- src/presenters/endpoints.mjs
+++ src/presenters/endpoints.mjs
@@ -1,4 +1,5 @@
 export class Endpoints {
     static SEARCH_URL = "https://www.5dwm.mydns.jp:8181/5dtest/QuerySearch";
+    static CATEGORIES_URL = "https://5dworldmap.com/api/v1/categories";
     static USERS_URL = "https://5dworldmap.com/api/v1/users";
 }
