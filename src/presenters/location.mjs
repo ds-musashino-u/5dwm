8ba5d4af37d7b196dfb8d725637ca944e5042da6
Milch <kawatan@gmail.com>
2022-03-22 13:47:50 +0900

    Create location.mjs

---
+++ src/presenters/location.mjs
@@ -0,0 +1,15 @@
+/**
+ * @classdesc Location
+ */
+export class Location {
+    /**
+     * @param {!number} longitude - Longitude
+     * @param {!number} latitude - Latitude
+     * @param {?string} address - Address
+     */
+    constructor(longitude, latitude, address = null) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.updatedAt = new Date(updatedAt);
+    }
+}
