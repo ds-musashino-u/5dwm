4dde49963d5f0d891f28170de1c551e981528487
Milch <kawatan@gmail.com>
2022-03-22 14:28:01 +0900

    Update search.mjs

--- src/presenters/search.mjs
+++ src/presenters/search.mjs
@@ -5,7 +5,7 @@ import { Location } from "./location.mjs";
 /**
  * @classdesc SearchItem
  */
- export class SearchItem {
+export class SearchItem {
     /**
      * @param {!number} id - Identifier
      * @param {?string} description - Description
@@ -32,6 +32,36 @@ import { Location } from "./location.mjs";
  * @param {?number} limit - Limit
  * @return {Array<SearchItem>} - Array of search items
  */
- export async function search(keywords, offset = 0, limit = null) {
+export async function search(keywords, offset = 0, limit = null) {
+    const imageUrl = "";
+    const categories = "";
+    const kinds = "";
+    const databases = "";
 
- }
+    const response = await fetch("https://5dworldmap.com/api/v1/echo", {
+        mode: "cors",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            url: encodeURI(
+                `${Endpoints.SEARCH_URL}?imgurl=${imageUrl}&keyword=${keywords.join(",")}&ctg=${categories}&kind=${kinds}&db=${databases}`
+            ),
+        }),
+    });
+
+    if (response.ok) {
+        const searchItems = [];
+    
+        for (const item of await response.json()) {
+            if ("id" in item) {
+                searchItems.push(new SearchItem(item.id, item.description, [item.category], new Media(item.file_name, item.kind), new Location(item.lng, item.lat, item.place), item.datetaken));
+            }
+        }
+
+        return searchItems;
+    } else {
+        throw new Error(response.statusText);
+    }
+}
