fc6a4fbfc092cad56a680d6c17dbcf51fd1b2e69
Milch <kawatan@gmail.com>
2022-03-22 14:03:18 +0900

    Update search.mjs

--- src/presenters/search.mjs
+++ src/presenters/search.mjs
@@ -1,15 +1,37 @@
+import { Endpoints } from "./endpoints.mjs";
+import { Media } from "./media.mjs";
+import { Location } from "./location.mjs";
+
 /**
  * @classdesc SearchItem
  */
  export class SearchItem {
     /**
      * @param {!number} id - Identifier
-     * @param {!string} name - Category name
-     * @param {!string} createdAt - Updated date time (ISO 8601)
+     * @param {?string} description - Description
+     * @param {!Array<string>} categories - Categories
+     * @param {!Media} media - Media
+     * @param {!Location} location - Location
+     * @param {!string} createdAt - Created date time (ISO 8601)
      */
-    constructor(id, name, location, createdAt) {
+    constructor(id, description, categories, media, location, createdAt) {
         this.id = id;
-        this.name = name;
-        this.updatedAt = new Date(updatedAt);
+        this.description = description;
+        this.categories = categories;
+        this.media = media;
+        this.location = location;
+        this.createdAt = new Date(createdAt);
     }
 }
+
+/**
+ * /api/v1/categories
+ * @module search
+ * @param {!Array<string>} - Keywords
+ * @param {!number} offset - Offset
+ * @param {?number} limit - Limit
+ * @return {Array<SearchItem>} - Array of search items
+ */
+ export async function search(keywords, offset = 0, limit = null) {
+
+ }
