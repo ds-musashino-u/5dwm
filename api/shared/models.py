75de0c5b0bf297c95ae446ec23db9860f919b183
Milch <kawatan@gmail.com>
2022-03-23 12:31:19 +0900

    Update models.py

--- api/shared/models.py
+++ api/shared/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, Float, String, Text, DateTime
+from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -6,16 +7,30 @@ Base = declarative_base()
 
 class Category(Base):
     __tablename__ = 'category'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer(), primary_key=True)
     name = Column('name', String(50))
-    updated_at = Column('update_time', DateTime)
+    updated_at = Column('update_time', DateTime())
+
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer(), primary_key=True)
+    url = Column('file_name', Text())
+    type = Column('kind', String(10))
+    categories = Column('category', ARRAY(Text()))
+    address = Column('place', Text())
+    description = Column('description', Text())
+    username = Column('user_cns', String(20))
+    latitude = Column('lat', Float())
+    longitude = Column('lng', Float())
+    created_at = Column('datetaken', DateTime())
 
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer(), primary_key=True)
     username = Column('user_cns', String(20))
     first_name = Column('firstname', String(20))
     last_name = Column('lastname', String(20))
-    updated_at = Column('update_time', DateTime)
+    updated_at = Column('update_time', DateTime())
     email = Column(String(128), default=None, nullable=True)
