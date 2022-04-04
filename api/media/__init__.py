af49d0414658a20ea1ddcf52d5118352a13c691c
Milch <kawatan@gmail.com>
2022-04-05 01:15:17 +0900

    Update __init__.py

--- api/media/__init__.py
+++ api/media/__init__.py
@@ -34,70 +34,71 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             '''
             pass
 
-        if req.headers.get('Content-Type') == 'application/json':
-            data = req.get_json()
-            mime_type = data.get('type')
-            sort = data['sort'] if 'sort' in data and data['sort'] is not None else 'created_at'
-            order = data['order'] if 'order' in data and data['order'] is not None else 'desc'
-            offset = data.get('offset')
-            limit = data.get('limit')
-
-        else:
-            mime_type = req.params['type'] if 'type' in req.params else None
-            sort = req.params['sort'] if 'sort' in req.params else 'created_at'
-            order = req.params['order'] if 'order' in req.params else 'desc'
-            offset = int(req.params['offset']
-                         ) if 'offset' in req.params else None
-            limit = int(req.params['limit']) if 'limit' in req.params else None
-
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
-        try:
-            media = []
-            query = session.query(Media)
-
-            if mime_type is not None:
-                query = query.filter(Media.type.like(mime_type.replace('*', '%')))
-
-            if sort == 'created_at':
-                if order is None:
-                    query = query.order_by(desc(Media.created_at))
-
-                else:
-                    if order == 'asc':
-                        query = query.order_by(Media.created_at)
-                    elif order == 'desc':
-                        query = query.order_by(desc(Media.created_at))
-                    else:
-                        return func.HttpResponse(status_code=400, mimetype='', charset='')
+        if req.method == 'GET':
+            if req.headers.get('Content-Type') == 'application/json':
+                data = req.get_json()
+                mime_type = data.get('type')
+                sort = data['sort'] if 'sort' in data and data['sort'] is not None else 'created_at'
+                order = data['order'] if 'order' in data and data['order'] is not None else 'desc'
+                offset = data.get('offset')
+                limit = data.get('limit')
 
             else:
-                return func.HttpResponse(status_code=400, mimetype='', charset='')
+                mime_type = req.params['type'] if 'type' in req.params else None
+                sort = req.params['sort'] if 'sort' in req.params else 'created_at'
+                order = req.params['order'] if 'order' in req.params else 'desc'
+                offset = int(req.params['offset']
+                            ) if 'offset' in req.params else None
+                limit = int(req.params['limit']) if 'limit' in req.params else None
 
-            if limit is not None:
-                query = query.limit(limit)
+            Session = sessionmaker(bind=engine)
+            session = Session()
 
-            if offset is not None:
-                query = query.offset(offset)
+            try:
+                media = []
+                query = session.query(Media)
 
-            for item in query.all():
-                media.append({
-                    'id': item.id,
-                    'url': item.url,
-                    'type': item.type,
-                    'categories': item.categories,
-                    'address': item.address,
-                    'description': item.description,
-                    'username': item.username,
-                    'location': {'type': 'Point', 'coordinates': [item.longitude, item.latitude]},
-                    'created_at': item.created_at.strftime('%Y-%m-%dT%H:%M:%SZ')
-                })
+                if mime_type is not None:
+                    query = query.filter(Media.type.like(mime_type.replace('*', '%')))
 
-            return func.HttpResponse(json.dumps(media), status_code=200, mimetype='application/json', charset='utf-8')
+                if sort == 'created_at':
+                    if order is None:
+                        query = query.order_by(desc(Media.created_at))
 
-        finally:
-            session.close()
+                    else:
+                        if order == 'asc':
+                            query = query.order_by(Media.created_at)
+                        elif order == 'desc':
+                            query = query.order_by(desc(Media.created_at))
+                        else:
+                            return func.HttpResponse(status_code=400, mimetype='', charset='')
+
+                else:
+                    return func.HttpResponse(status_code=400, mimetype='', charset='')
+
+                if limit is not None:
+                    query = query.limit(limit)
+
+                if offset is not None:
+                    query = query.offset(offset)
+
+                for item in query.all():
+                    media.append({
+                        'id': item.id,
+                        'url': item.url,
+                        'type': item.type,
+                        'categories': item.categories,
+                        'address': item.address,
+                        'description': item.description,
+                        'username': item.username,
+                        'location': {'type': 'Point', 'coordinates': [item.longitude, item.latitude]},
+                        'created_at': item.created_at.strftime('%Y-%m-%dT%H:%M:%SZ')
+                    })
+
+                return func.HttpResponse(json.dumps(media), status_code=200, mimetype='application/json', charset='utf-8')
+
+            finally:
+                session.close()
 
     except Exception as e:
         logging.error(f'{e}')
