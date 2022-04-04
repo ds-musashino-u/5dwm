86de102237d8092b75bda2788e9eead7d52bc976
Milch <kawatan@gmail.com>
2022-04-05 00:43:11 +0900

    Update media.mjs

--- src/presenters/media.mjs
+++ src/presenters/media.mjs
@@ -1,3 +1,4 @@
+import { Endpoints } from "./endpoints.mjs";
 import { Location } from "./location.mjs";
 
 /**
@@ -29,5 +30,55 @@ export class Media {
 
     get hasPreviewImageUrl() {
         return this.hasPreviewImageUrl !== null;
+    }
+}
+
+/**
+ * /api/v1/media
+ * @module getMedia
+ * @param {?string} type - MIME type
+ * @param {?string} sort - Sort
+ * @param {?string} order - Order (asc or desc)
+ * @param {!number} offset - Offset
+ * @param {?number} limit - Limit
+ * @return {Array<Media>} - Array of media items
+ */
+ export async function getMedia(type = null, sort = null, order = null, offset = 0, limit = null) {
+    let url = `${Endpoints.MEDIA_URL}?offset=${offset}`;
+
+    if (limit !== null) {
+        url += `&limit=${limit}`
+    }
+
+    if (type !== null) {
+        url += `&type=${type}`
+    }
+
+    if (sort !== null) {
+        url += `&sort=${sort}`
+    }
+
+    if (order !== null) {
+        url += `&order=${order}`
+    }
+
+    const response = await fetch(encodeURI(url), {
+        mode: "cors",
+        method: "GET",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+    });
+
+    if (response.ok) {
+        const media = [];
+
+        for (const item of await response.json()) {
+            media.push(new Media(item.id, item.url, item.type, item.categories, item.description, item.username, new Location(item.longitude, item.latitude, item.address), item.created_at));
+        }
+
+        return media;
+    } else {
+        throw new Error(response.statusText);
     }
 }
