37095f8495e9c7fbabdad5b0b43e58efe733e78a
Milch <kawatan@gmail.com>
2022-04-05 00:43:15 +0900

    Create media.test.mjs

---
+++ test/media.test.mjs
@@ -0,0 +1,24 @@
+import assert from "assert";
+import sinon from "sinon";
+import { getMedia } from "../src/presenters/media.mjs"
+
+global.fetch = () => { };
+global.window = {
+    btoa: value => value
+};
+
+describe("media", function () {
+    let fetchStub;
+
+    beforeEach(() => {
+        fetchStub = sinon.stub(global, 'fetch');
+    });
+    afterEach(() => {
+        fetchStub.restore();
+    });
+    it("getMedia", async function () {
+        fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve([{ id: 1, url: "https://5dworldmap.com/foobar.png", type: "image/png", categories: ["foo", "bar"], address: "foo", description: "foo bar baz", username: "foobar", latitude: 21.028344772352863, longitude: 105.85271637244875, created_at: "1970-01-01T09:00:00Z" }]) }));
+
+        assert.equal(true, Array.isArray(await getMedia("image/png")));
+    });
+});
