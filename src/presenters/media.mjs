ba646397e55abc6f8c012bdd693806f14f47c63e
Milch <kawatan@gmail.com>
2022-04-04 17:02:31 +0900

    Update media.mjs

--- src/presenters/media.mjs
+++ src/presenters/media.mjs
@@ -1,15 +1,29 @@
+import { Location } from "./location.mjs";
+
 /**
  * @classdesc Media
  */
 export class Media {
     /**
+     * @param {!integer} id - Media Identifier
      * @param {!string} url - Media URL
      * @param {!string} type - MIME type of Media
+     * @param {!Array<string>} categories - Categories
+     * @param {!string} description - Description
+     * @param {!string} username - User name
+     * @param {!Location} location - Location
+     * @param {!string} createdAt - Updated date time (ISO 8601)
      * @param {?string} previewImageUrl - Preview image URL
      */
-    constructor(url, type, previewImageUrl = null) {
+    constructor(id, url, type, categories, description, username, location, createdAt, previewImageUrl = null) {
+        this.id = id;
         this.url = url;
         this.type = type;
+        this.categories = categories;
+        this.description = description;
+        this.username = username;
+        this.location = location;
+        this.createdAt = new Date(createdAt);
         this.previewImageUrl = previewImageUrl;
     }
 
