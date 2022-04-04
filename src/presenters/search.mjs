b39ce6cf1dc450b938997142f43f0311e01b0f11
Milch <kawatan@gmail.com>
2022-04-05 00:43:13 +0900

    Update search.mjs

--- src/presenters/search.mjs
+++ src/presenters/search.mjs
@@ -3,14 +3,19 @@ import { Media } from "./media.mjs";
 import { Location } from "./location.mjs";
 
 /**
- * /api/v1/categories
+ * /api/v1/search
  * @module search
- * @param {!Array<string>} - Keywords
+ * @param {!Array<string>} keywords - Keywords
+ * @param {!Array<string>} categories - Categories
+ * @param {!Array<string>} types - Types
+ * @param {?string} imageUrl - Image URL
+ * @param {?string} sort - Sort
+ * @param {?string} order - Order (asc or desc)
  * @param {!number} offset - Offset
  * @param {?number} limit - Limit
- * @return {Array<SearchItem>} - Array of search items
+ * @return {Array<Media>} - Array of media items
  */
-export async function search(keywords, categories, types, imageUrl = null, offset = 0, limit = null) {
+export async function search(keywords, categories, types, imageUrl = null, sort = null, order = null, offset = 0, limit = null) {
     const data = {
         keywords: keywords,
         categories: categories,
@@ -21,6 +26,14 @@ export async function search(keywords, categories, types, imageUrl = null, offse
 
     if (imageUrl !== null) {
         data["image_url"] = imageUrl;
+    }
+
+    if (sort !== null) {
+        data["sort"] = sort;
+    }
+
+    if (order !== null) {
+        data["order"] = order;
     }
 
     const response = await fetch(Endpoints.SEARCH_URL, {
