c3e6474442fc15c88328dadbf2643cb6d9aacdf2
Milch <kawatan@gmail.com>
2022-04-05 16:21:19 +0900

    Update categories.test.mjs

--- test/categories.test.mjs
+++ test/categories.test.mjs
@@ -1,6 +1,6 @@
 import assert from "assert";
 import sinon from "sinon";
-import { getCategories } from "../src/presenters/categories.mjs"
+import { Category, getCategories, getCategory, addCategory, removeCategory } from "../src/presenters/categories.mjs"
 
 global.fetch = () => { };
 global.window = {
@@ -20,5 +20,20 @@ describe("users", function () {
         fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve([{ id: "1", name: "foobar", updated_at: "1970-01-01T09:00:00Z" }]) }));
 
         assert.equal(true, Array.isArray(await getCategories()));
+    });
+    it("getCategory", async function () {
+        fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve({ id: "1", name: "foobar", updated_at: "1970-01-01T09:00:00Z" }) }));
+
+        assert.equal(true, await getCategory(1) instanceof Category);
+    });
+    it("addCategory", async function () {
+        fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve({ id: "1", name: "foobar", updated_at: "1970-01-01T09:00:00Z" }) }));
+
+        assert.equal(true, await addCategory("foobar") instanceof Category);
+    });
+    it("removeCategory", async function () {
+        fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve({ id: "1", name: "foobar", updated_at: "1970-01-01T09:00:00Z" }) }));
+
+        assert.equal(true, await removeCategory(1) instanceof Category);
     });
 });
