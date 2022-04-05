50f938b76f7e13667a2949b176a51a28f1f92ea6
Milch <kawatan@gmail.com>
2022-04-05 13:54:15 +0900

    Add media function

---
+++ api/medium/__init__.py
@@ -0,0 +1,93 @@
+import json
+import logging
+import os
+from urllib.request import urlopen, Request
+from sqlalchemy import create_engine, desc
+from sqlalchemy.orm import sessionmaker
+from shared.models import Media
+
+import azure.functions as func
+
+
+engine = create_engine(os.environ.get('POSTGRESQL_CONNECTION_URL'), connect_args={
+                       'sslmode': 'require'}, pool_recycle=60)
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        if 'Authorization' in req.headers:
+            '''
+            jwt = req.headers['Authorization'].split(' ')[1].split('.') if req.headers['Authorization'].startswith('Bearer ') else req.headers['Authorization'].split('.')
+
+            if json.loads(b64decode(jwt[0] + '=' * (-len(jwt[0]) % 4)))['typ'] == 'JWT' and json.loads(b64decode(jwt[1] + '=' * (-len(jwt[1]) % 4)))['iss'] == 'https://':
+                try:
+                    response = urlopen(Request(
+                        f'https://',
+                        headers={'Content-Type': 'application/json'},
+                        data=json.dumps({'idToken': req.headers['Authorization']}).encode('utf-8')))
+
+                    if response.getcode() != 200:
+                        raise Exception
+
+                except Exception:
+                    return func.HttpResponse(status_code=403, mimetype='', charset='')
+            '''
+            pass
+
+        id = int(req.route_params.get('id'))
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        if req.method == 'DELETE':
+            try:
+                media = session.query(Media).filter(Media.id == id).one()
+                
+                session.delete(media)
+                session.commit()
+
+                return func.HttpResponse(json.dumps({
+                    'id': media.id,
+                    'url': media.url,
+                    'type': media.type,
+                    'categories': media.categories,
+                    'address': media.address,
+                    'description': media.description,
+                    'username': media.username,
+                    'location': {'type': 'Point', 'coordinates': [media.longitude, media.latitude]},
+                    'created_at': media.created_at.strftime('%Y-%m-%dT%H:%M:%SZ')
+                }), status_code=200, mimetype='application/json', charset='utf-8')
+
+            finally:
+                session.rollback()
+                session.close()
+
+        else:
+            try:
+                media = session.query(Media).filter(Media.id == id).one()
+
+                return func.HttpResponse(json.dumps({
+                    'id': media.id,
+                    'url': media.url,
+                    'type': media.type,
+                    'categories': media.categories,
+                    'address': media.address,
+                    'description': media.description,
+                    'username': media.username,
+                    'location': {'type': 'Point', 'coordinates': [media.longitude, media.latitude]},
+                    'created_at': media.created_at.strftime('%Y-%m-%dT%H:%M:%SZ')
+                }), status_code=200, mimetype='application/json', charset='utf-8')
+
+            finally:
+                session.close()
+
+    except Exception as e:
+        logging.error(f'{e}')
+
+        return func.HttpResponse(json.dumps({
+            'error': {
+                'message': str(e),
+                'type': type(e).__name__}
+        }),
+            status_code=400,
+            mimetype='application/json',
+            charset='utf-8')
