2e56c9658eba79270f5f26288361d64fdbb657b7
Milch <kawatan@gmail.com>
2022-04-05 13:54:58 +0900

    Update media.test.mjs

--- test/media.test.mjs
+++ test/media.test.mjs
@@ -17,7 +17,7 @@ describe("media", function () {
         fetchStub.restore();
     });
     it("getMedia", async function () {
-        fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve([{ id: 1, url: "https://5dworldmap.com/foobar.png", type: "image/png", categories: ["foo", "bar"], address: "foo", description: "foo bar baz", username: "foobar", latitude: 21.028344772352863, longitude: 105.85271637244875, created_at: "1970-01-01T09:00:00Z" }]) }));
+        fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve([{ id: 1, url: "https://5dworldmap.com/foobar.png", type: "image/png", categories: ["foo", "bar"], address: "foo", description: "foo bar baz", username: "foobar", location: { type: "Point", coordinates: [105.85271637244875, 21.028344772352863,] }, created_at: "1970-01-01T09:00:00Z" }]) }));
 
         assert.equal(true, Array.isArray(await getMedia("image/png")));
     });
