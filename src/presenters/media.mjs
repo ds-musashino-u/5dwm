d726a6c9157089a302c30852d7d4d3c05a417f95
Milch <kawatan@gmail.com>
2022-04-05 20:14:41 +0900

    Update media.mjs

--- src/presenters/media.mjs
+++ src/presenters/media.mjs
@@ -112,3 +112,76 @@ export async function getMedium(id) {
         throw new Error(response.statusText);
     }
 }
+
+/**
+ * /api/v1/media
+ * @module addMedium
+ * @param {!string} url - URL 
+ * @param {!string} type - MIME type
+ * @param {!Array<string>} categories - Categories
+ * @param {!string} description - Description
+ * @param {!string} username - User name
+ * @param {!Location} location - Location
+ * @return {?Media} - Media item
+ */
+ export async function addMedium(url, type, categories, description, username, location) {
+    const response = await fetch(encodeURI(Endpoints.MEDIA_URL), {
+        mode: "cors",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            url: url,
+            type: type,
+            categories: categories,
+            description: description,
+            username: username,
+            location: {
+                type: "Point",
+                coordinates: [location.longitude, location.latitude]
+            },
+            address: location.address
+        })
+    });
+
+    if (response.ok) {
+        const item = await response.json()
+
+        if (item === null) {
+            return null;
+        }
+
+        return new Media(item.id, item.url, item.type, item.categories, item.description, item.username, new Location(item.location.coordinates[0], item.location.coordinates[1], item.address), item.created_at);
+    } else {
+        throw new Error(response.statusText);
+    }
+}
+
+/**
+ * /api/v1/media/{id}
+ * @module removeMedium
+ * @param {!number} id - Media identifier
+ * @return {?Media} - Media item
+ */
+export async function removeMedium(id) {
+    const response = await fetch(encodeURI(`${Endpoints.MEDIA_URL}/${id}`), {
+        mode: "cors",
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+    });
+
+    if (response.ok) {
+        const item = await response.json()
+
+        if (item === null) {
+            return null;
+        }
+
+        return new Media(item.id, item.url, item.type, item.categories, item.description, item.username, new Location(item.location.coordinates[0], item.location.coordinates[1], item.address), item.created_at);
+    } else {
+        throw new Error(response.statusText);
+    }
+}
