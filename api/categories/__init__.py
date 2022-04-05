97ea2ffa90dcd69112b1d7adfcf33dd246ed9e83
Milch <kawatan@gmail.com>
2022-04-05 13:54:36 +0900

    Update __init__.py

--- api/categories/__init__.py
+++ api/categories/__init__.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+from datetime import datetime, timezone
 from urllib.request import urlopen, Request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -34,40 +35,69 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             '''
             pass
 
-        if req.headers.get('Content-Type') == 'application/json':
-            data = req.get_json()
-            offset = data.get('offset')
-            limit = data.get('limit')
+        if req.method == 'GET':
+            if req.headers.get('Content-Type') == 'application/json':
+                data = req.get_json()
+                offset = data.get('offset')
+                limit = data.get('limit')
 
-        else:
-            offset = int(req.params['offset']
-                         ) if 'offset' in req.params else None
-            limit = int(req.params['limit']) if 'limit' in req.params else None
+            else:
+                offset = int(req.params['offset']
+                             ) if 'offset' in req.params else None
+                limit = int(req.params['limit']
+                            ) if 'limit' in req.params else None
 
-        Session = sessionmaker(bind=engine)
-        session = Session()
+            Session = sessionmaker(bind=engine)
+            session = Session()
 
-        try:
-            categories = []
-            query = session.query(Category).order_by(Category.id)
+            try:
+                categories = []
+                query = session.query(Category).order_by(Category.id)
 
-            if limit is not None:
-                query = query.limit(limit)
+                if limit is not None:
+                    query = query.limit(limit)
 
-            if offset is not None:
-                query = query.offset(offset)
+                if offset is not None:
+                    query = query.offset(offset)
 
-            for category in query.all():
-                categories.append({
+                for category in query.all():
+                    categories.append({
+                        'id': category.id,
+                        'name': category.name,
+                        'updated_at': category.updated_at.strftime('%Y-%m-%dT%H:%M:%SZ')
+                    })
+
+                return func.HttpResponse(json.dumps(categories), status_code=200, mimetype='application/json', charset='utf-8')
+
+            finally:
+                session.close()
+
+        elif req.method == 'POST' and req.headers.get('Content-Type') == 'application/json':
+            name = req.get_json()['name']
+
+            if type(name) != str:
+                return func.HttpResponse(status_code=400, mimetype='', charset='')
+
+            Session = sessionmaker(bind=engine)
+            session = Session()
+
+            try:
+                category = Category()
+                category.name = name
+                category.updated_at = datetime.now(timezone.utc)
+
+                session.add(category)
+                session.commit()
+
+                return func.HttpResponse(json.dumps({
                     'id': category.id,
                     'name': category.name,
                     'updated_at': category.updated_at.strftime('%Y-%m-%dT%H:%M:%SZ')
-                })
+                }), status_code=200, mimetype='application/json', charset='utf-8')
 
-            return func.HttpResponse(json.dumps(categories), status_code=200, mimetype='application/json', charset='utf-8')
-
-        finally:
-            session.close()
+            finally:
+                session.rollback()
+                session.close()
 
     except Exception as e:
         logging.error(f'{e}')
