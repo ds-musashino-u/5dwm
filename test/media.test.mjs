afce129fdb54a532be876a36351f59f676f8b777
Milch <kawatan@gmail.com>
2022-04-05 16:21:22 +0900

    Update media.test.mjs

--- test/media.test.mjs
+++ test/media.test.mjs
@@ -1,6 +1,6 @@
 import assert from "assert";
 import sinon from "sinon";
-import { getMedia } from "../src/presenters/media.mjs"
+import { Media, getMedia, getMedium } from "../src/presenters/media.mjs"
 
 global.fetch = () => { };
 global.window = {
@@ -20,5 +20,10 @@ describe("media", function () {
         fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve([{ id: 1, url: "https://5dworldmap.com/foobar.png", type: "image/png", categories: ["foo", "bar"], address: "foo", description: "foo bar baz", username: "foobar", location: { type: "Point", coordinates: [105.85271637244875, 21.028344772352863] }, created_at: "1970-01-01T09:00:00Z" }]) }));
 
         assert.equal(true, Array.isArray(await getMedia("image/png")));
+    });
+    it("getMedium", async function () {
+        fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve({ id: 1, url: "https://5dworldmap.com/foobar.png", type: "image/png", categories: ["foo", "bar"], address: "foo", description: "foo bar baz", username: "foobar", location: { type: "Point", coordinates: [105.85271637244875, 21.028344772352863] }, created_at: "1970-01-01T09:00:00Z" }) }));
+
+        assert.equal(true, await getMedium(1) instanceof Media);
     });
 });
