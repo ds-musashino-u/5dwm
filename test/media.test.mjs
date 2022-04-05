e533e3ce61cebcdd8101246c86447b4859ec1f38
Milch <kawatan@gmail.com>
2022-04-05 20:37:01 +0900

    Update media.test.mjs

--- test/media.test.mjs
+++ test/media.test.mjs
@@ -1,7 +1,7 @@
 import assert from "assert";
 import sinon from "sinon";
 import { Location } from "../src/presenters/location.mjs";
-import { Media, getMedia, getMedium, addMedium, removeMedium } from "../src/presenters/media.mjs"
+import { Media, getMedia, getMedium, insertMedium, deleteMedium } from "../src/presenters/media.mjs"
 
 global.fetch = () => { };
 global.window = {
@@ -27,14 +27,14 @@ describe("media", function () {
 
         assert.equal(true, await getMedium(1) instanceof Media);
     });
-    it("addMedium", async function () {
+    it("insertMedium", async function () {
         fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve({ id: 1, url: "https://5dworldmap.com/foobar.png", type: "image/png", categories: ["foo", "bar"], address: "foo", description: "foo bar baz", username: "foobar", location: { type: "Point", coordinates: [105.85271637244875, 21.028344772352863] }, created_at: "1970-01-01T09:00:00Z" }) }));
 
-        assert.equal(true, await addMedium("https://5dworldmap.com/foobar.png", "image/png", ["foo", "bar"], "foo bar baz", "foobar", new Location(105.85271637244875, 21.028344772352863, "foo")) instanceof Media);
+        assert.equal(true, await insertMedium("https://5dworldmap.com/foobar.png", "image/png", ["foo", "bar"], "foo bar baz", "foobar", new Location(105.85271637244875, 21.028344772352863, "foo")) instanceof Media);
     });
-    it("removeMedium", async function () {
+    it("deleteMedium", async function () {
         fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve({ id: 1, url: "https://5dworldmap.com/foobar.png", type: "image/png", categories: ["foo", "bar"], address: "foo", description: "foo bar baz", username: "foobar", location: { type: "Point", coordinates: [105.85271637244875, 21.028344772352863] }, created_at: "1970-01-01T09:00:00Z" }) }));
 
-        assert.equal(true, await removeMedium(1) instanceof Media);
+        assert.equal(true, await deleteMedium(1) instanceof Media);
     });
 });
