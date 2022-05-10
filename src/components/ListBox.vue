4c15ff0e92ae76936d4495b3b46d0f474f5397eb
Milch <kawatan@gmail.com>
2022-05-10 15:34:02 +0900

    Update ListBox.vue

--- src/components/ListBox.vue
+++ src/components/ListBox.vue
@@ -1,7 +1,7 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import { ref, reactive, toRef, watch, watchEffect } from "vue";
+import { ref, reactive, watch } from "vue";
 
 const props = defineProps({
   name: { type: String, required: false, default: null },
