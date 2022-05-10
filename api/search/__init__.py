c13552eb98d396108265e8b5e86e260f81926339
Milch <kawatan@gmail.com>
2022-05-10 19:23:59 +0900

    Update __init__.py

--- api/search/__init__.py
+++ api/search/__init__.py
@@ -21,7 +21,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         if 'Authorization' in req.headers and req.headers['X-Authorization'].startswith('Bearer '):
             if not verify(req.headers['X-Authorization'].split(' ')[1], os.environ['AUTH0_JWKS_URL'], os.environ['AUTH0_AUDIENCE'], os.environ['AUTH0_ISSUER'], [os.environ['AUTH0_ALGORITHM']]):
-                return func.HttpResponse(status_code=403, mimetype='', charset='')
+                return func.HttpResponse(status_code=401, mimetype='', charset='')
 
         '''
         with psycopg2.connect(os.environ.get('POSTGRESQL_CONNECTION_URL'), sslmode='disable') as connection: #require
