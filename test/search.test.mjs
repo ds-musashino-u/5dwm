b8b503a4930ba7edd9073f9f14229dad01f3636a
Milch <kawatan@gmail.com>
2022-05-11 18:47:56 +0900

    Update search.test.mjs

--- test/search.test.mjs
+++ test/search.test.mjs
@@ -19,7 +19,7 @@ describe("search", function () {
     it("search", async function () {
         fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve({ count: 1, items: [{ id: 1, url: "https://5dworldmap.com/foobar.png", type: "image/png", categories: ["foo", "bar"], address: "foo", description: "foo bar baz", username: "foobar", location: { type: "Point", coordinates: [105.85271637244875, 21.028344772352863] }, created_at: "1970-01-01T09:00:00Z" }] }) }));
 
-        const [items, count] = await search("token", ["foo"], ["bar"], ["baz"]);
+        const [items, count] = await search("dummy_token", ["foo"], ["bar"], ["baz"]);
 
         assert.equal(true, Array.isArray(items));
         assert.equal(true, typeof (count) === "number");
