ffde32e90ed4df7c32be505d582b2df07876cf01
Milch <kawatan@gmail.com>
2022-05-11 16:15:50 +0900

    Update search.test.mjs

--- test/search.test.mjs
+++ test/search.test.mjs
@@ -17,8 +17,11 @@ describe("search", function () {
         fetchStub.restore();
     });
     it("search", async function () {
-        fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve([{ id: 1, url: "https://5dworldmap.com/foobar.png", type: "image/png", categories: ["foo", "bar"], address: "foo", description: "foo bar baz", username: "foobar", location: { type: "Point", coordinates: [105.85271637244875, 21.028344772352863] }, created_at: "1970-01-01T09:00:00Z" }]) }));
+        fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve({ count: 1, items: [{ id: 1, url: "https://5dworldmap.com/foobar.png", type: "image/png", categories: ["foo", "bar"], address: "foo", description: "foo bar baz", username: "foobar", location: { type: "Point", coordinates: [105.85271637244875, 21.028344772352863] }, created_at: "1970-01-01T09:00:00Z" }] }) }));
 
-        assert.equal(true, Array.isArray(await search("token", ["foo"], ["bar"], ["baz"])));
+        const [items, count] = await search("token", ["foo"], ["bar"], ["baz"]);
+
+        assert.equal(true, Array.isArray(items));
+        assert.equal(true, typeof (count) === "number");
     });
 });
