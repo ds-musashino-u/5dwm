c5da15b0d0e5c9261cde17be228297b3e9146cb8
Milch <kawatan@gmail.com>
2022-05-13 18:56:45 +0900

    Update search.mjs

--- src/presenters/search.mjs
+++ src/presenters/search.mjs
@@ -9,6 +9,7 @@ import { Location } from "./location.mjs";
  * @param {!Array<string>} keywords - Keywords
  * @param {!Array<string>} categories - Categories
  * @param {!Array<string>} types - Types
+ * @param {!Array<string>} usernames - Usernames
  * @param {?string} imageUrl - Image URL
  * @param {?string} sort - Sort
  * @param {?string} order - Order (asc or desc)
@@ -16,11 +17,12 @@ import { Location } from "./location.mjs";
  * @param {?number} limit - Limit
  * @return {Array<Media>} - Array of media items
  */
-export async function search(token, keywords, categories, types, imageUrl = null, sort = null, order = null, offset = 0, limit = null) {
+export async function search(token, keywords, categories, types, usernames, imageUrl = null, sort = null, order = null, offset = 0, limit = null) {
     const data = {
         keywords: keywords,
         categories: categories,
         types: types,
+        usernames: usernames,
         offset: offset,
         limit: limit
     };
@@ -52,8 +54,10 @@ export async function search(token, keywords, categories, types, imageUrl = null
         const json = await response.json();
 
         for (const item of json.items) {
-            if (item.location.type === "Point") {
+            if (item.location !== null && item.location.type === "Point" && typeof(item.location.coordinates[0]) === "number" && typeof(item.location.coordinates[1]) === "number") {
                 media.push(new Media(item.id, item.url, item.type, item.categories, item.description, item.username, new Location(item.location.coordinates[0], item.location.coordinates[1], item.address), item.created_at));
+            } else {
+                media.push(new Media(item.id, item.url, item.type, item.categories, item.description, item.username, null, item.created_at));
             }
         }
 
