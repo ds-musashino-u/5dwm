626637c37a2b0637ef4fa541848527f54c94488d
Milch <kawatan@gmail.com>
2022-05-13 17:23:33 +0900

    Update endpoints.mjs

--- src/presenters/endpoints.mjs
+++ src/presenters/endpoints.mjs
@@ -1,6 +1,7 @@
 export class Endpoints {
     static SEARCH_URL = "https://5dworldmap.com/api/v1/search";
     static CATEGORIES_URL = "https://5dworldmap.com/api/v1/categories";
+    static TYPES_URL = "https://5dworldmap.com/api/v1/types";
     static MEDIA_URL = "https://5dworldmap.com/api/v1/media";
     static USERS_URL = "https://5dworldmap.com/api/v1/users";
 }
