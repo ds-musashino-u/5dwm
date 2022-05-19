02f1c0ea44826d1158af9209591438b19cea26d1
Milch <kawatan@gmail.com>
2022-05-19 11:17:29 +0900

    Update Preview.vue

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -14,12 +14,30 @@ const props = defineProps({
     <nav class="level is-mobile">
       <div class="level-left">
         <div class="level-item">
-          <span class="icon is-small">
-              <i class="fa-solid fa-file"></i>
-            </span>
+          <span class="icon is-small" v-if="item.type.startsWith('image')">
+            <i class="fa-solid fa-file-image"></i>
+          </span>
+          <span class="icon is-small" v-else-if="item.type.startsWith('video')">
+            <i class="fa-solid fa-file-video"></i>
+          </span>
+          <span class="icon is-small" v-else-if="item.type.startsWith('audio')">
+            <i class="fa-solid fa-file-audio"></i>
+          </span>
+          <span class="icon is-small" v-else-if="item.type.startsWith('text')">
+            <i class="fa-solid fa-file-lines"></i>
+          </span>
+          <span class="icon is-small" v-else>
+            <i class="fa-solid fa-file"></i>
+          </span>
         </div>
       </div>
       <div class="level-right">
+        <div class="level-item">
+          <a class="button is-rounded" target="_blank" v-bind:href="item.url"
+            ><span class="icon is-small"
+              ><i class="fa-solid fa-link"></i></span
+          ></a>
+        </div>
         <div class="level-item">
           <button
             class="button toggle is-rounded"
@@ -36,18 +54,43 @@ const props = defineProps({
       </div>
     </nav>
     <transition name="fade" mode="out-in">
-      <div class="control" v-if="!isCollapsed && item === null" key="loading">
+      <div
+        class="control"
+        v-if="!isCollapsed && item.type.startsWith('image')"
+        key="image"
+      >
         <nav class="level">
           <div class="level-item">
-            <span class="icon">
-              <i class="fas fa-spinner updating"></i>
-            </span>
+            <article class="media">
+              <div class="media-content">
+                <picture class="image">
+                  <img v-bind:src="item.url" v-bind:alt="String(index)" />
+                </picture>
+              </div>
+            </article>
           </div>
         </nav>
       </div>
-      <div class="control" v-else-if="!isCollapsed" key="loaded">
-      </div>
     </transition>
+  </div>
+  <div class="panel-block">
+    <div class="level">
+      <div class="level-left">
+        <div class="level-item">
+          <span class="is-size-6 is-uppercase has-text-weight-bold has-text-grey"
+            >Type</span
+          >
+        </div>
+      </div>
+      <div class="level-right">
+        <div class="level-item">
+          <span
+            class="is-size-6 has-text-weight-bold"
+            v-text="item.type"
+          ></span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -83,19 +126,21 @@ const props = defineProps({
     }
 
     > .level-right > .level-item {
-      button.is-rounded {
+      .button.is-rounded {
         border-radius: 9999px !important;
         padding: 12px !important;
         box-shadow: none !important;
-
-        > span {
-          transform: rotate(180deg);
-        }
 
         > span.icon {
           margin: 0 !important;
           width: 1rem !important;
           height: 1rem !important;
+        }
+      }
+
+      .button.toggle {
+        > span {
+          transform: rotate(180deg);
         }
 
         > span.collapsed {
@@ -111,78 +156,31 @@ const props = defineProps({
     flex-direction: column;
     justify-content: flex-start;
     align-items: center;
+    padding: 0 !important;
     width: 320px;
 
     .level {
-      padding: 0.5em 0.75em;
+      padding: 0;
       width: 100%;
-    }
 
-    .gallery {
-      display: flex;
-      margin: -2px -2px -2px -2px;
-      flex-direction: row;
-      flex-wrap: wrap;
-      justify-content: space-between;
-      align-items: flex-start;
-      width: calc(100% + 4px);
-
-      .media {
+      > .level-item > .media {
         display: inline-block;
-        margin: 2px 2px 2px 2px;
+        margin: 0;
         border: 0px none transparent !important;
         padding: 0;
-        width: calc(25% - 4px);
+        width: 100%;
 
         .media-content {
           width: 100%;
 
-          button {
-            z-index: 1;
-            position: relative;
-            margin: 0 !important;
-            padding: 0 !important;
+          picture {
+            margin: 0;
+            padding: 0;
             width: 100%;
-            aspect-ratio: 1 / 1;
-            border-radius: 8px;
-            box-shadow: none !important;
-            overflow: hidden;
-            background: transparent !important;
-
-            picture {
-              margin: 0;
-              padding: 0;
-              width: 100%;
-              height: 100%;
-
-              img {
-                object-fit: cover;
-                width: 100%;
-                height: 100%;
-              }
-            }
-
-            .icon {
-              position: absolute;
-              top: 50%;
-              left: 50%;
-              margin: 0 !important;
-              width: 1rem !important;
-              height: 1rem !important;
-              transform: translate(-50%, -50%);
-            }
-          }
-        }
-
-        figure {
-          margin: 0;
-          padding: 0;
-
-          .image {
-            overflow: hidden;
+            height: 100%;
 
             img {
-              object-fit: cover;
+              object-fit: contain;
               width: 100%;
               height: 100%;
             }
