901155178b818aaf7c94a94e5928428f640999d1
Milch <kawatan@gmail.com>
2022-05-23 12:48:52 +0900

    Update models.py

--- api/shared/models.py
+++ api/shared/models.py
@@ -25,7 +25,7 @@ class Media(Base):
     latitude = Column('lat', Float())
     longitude = Column('lng', Float())
     created_at = Column('datetaken', DateTime())
-    vector = relationship('ImageVector')
+    vector = relationship('ImageVector', uselist=True)
 
 
 class ImageVector(Base):
