6aaaf96a1dd6c6abd7cc6979865b0de5482a9d05
Milch <kawatan@gmail.com>
2022-05-24 19:51:48 +0900

    Update __init__.py

--- api/search/__init__.py
+++ api/search/__init__.py
@@ -112,18 +112,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                     score = None
 
                     if histogram is not None and item.vector is not None:
-                        vector1 = []
-                        vector2 = []
-
+                        sum = 0.0
+                        
                         for (index, value) in histogram:
                             for element in item.vector:
                                 if f'f{index}' == element.feature:
-                                    vector1.append(value)
-                                    vector2.append(element.value)
+                                    sum += value * element.value
 
-                        if len(vector1) > 0:
-                            score = np.dot(np.array(vector1),
-                                           np.array(vector2))
+                        if sum > 0.0:
+                            score = sum
 
                     media.append({
                         'id': item.id,
