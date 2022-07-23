e5b40f84483455661a18b841e385f2e2474a0e46
Milch <kawatan@gmail.com>
2022-07-23 12:28:38 +0900

    Update Preview.vue

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -7,6 +7,14 @@ const props = defineProps({
   isCollapsed: { type: Boolean, required: false, default: false },
   item: { type: Object, required: false, default: null },
 });
+const emit = defineEmits(["load", "unload"]);
+
+const load = (event, item) => {
+  emit("load", item);
+};
+const unload = (event, item) => {
+  emit("unload", item);
+};
 </script>
 
 <template>
@@ -14,26 +22,58 @@ const props = defineProps({
     <nav class="level is-mobile">
       <div class="level-left">
         <div class="level-item">
-          <span class="icon is-small" v-if="item.media.type.startsWith('image')">
+          <span
+            class="icon is-small"
+            v-if="item.media.type.startsWith('image')"
+          >
             <i class="fa-solid fa-file-image"></i>
           </span>
-          <span class="icon is-small" v-else-if="item.media.type.startsWith('video')">
+          <span
+            class="icon is-small"
+            v-else-if="item.media.type.startsWith('video')"
+          >
             <i class="fa-solid fa-file-video"></i>
           </span>
-          <span class="icon is-small" v-else-if="item.media.type.startsWith('audio')">
+          <span
+            class="icon is-small"
+            v-else-if="item.media.type.startsWith('audio')"
+          >
             <i class="fa-solid fa-file-audio"></i>
           </span>
-          <span class="icon is-small" v-else-if="item.media.type.startsWith('text')">
+          <span
+            class="icon is-small"
+            v-else-if="item.media.type.startsWith('text')"
+          >
             <i class="fa-solid fa-file-lines"></i>
           </span>
           <span class="icon is-small" v-else>
             <i class="fa-solid fa-file"></i>
           </span>
         </div>
+        <div class="level-item">
+          <button
+            class="button toggle"
+            type="button"
+            v-if="item.media.type.startsWith('kml') || item.media.type.startsWith('kmz')"
+            @click="item.loaded ? unload($event, item) : load($event, item)"
+          >
+            <transition name="fade" mode="out-in">
+              <span class="icon" v-if="item.loaded" key="on">
+                <i class="fa-solid fa-toggle-on"></i>
+              </span>
+              <span class="icon" v-else key="off">
+                <i class="fa-solid fa-toggle-off"></i>
+              </span>
+            </transition>
+          </button>
+        </div>
       </div>
       <div class="level-right">
         <div class="level-item">
-          <a class="button is-rounded" target="_blank" v-bind:href="item.media.url"
+          <a
+            class="button is-rounded"
+            target="_blank"
+            v-bind:href="item.media.url"
             ><span class="icon is-small"><i class="fa-solid fa-link"></i></span
           ></a>
         </div>
@@ -55,7 +95,11 @@ const props = defineProps({
     <transition name="fade" mode="out-in">
       <div
         class="control"
-        v-if="!isCollapsed && item.media.type.startsWith('image') && item.media.url.startsWith('https://')"
+        v-if="
+          !isCollapsed &&
+          item.media.type.startsWith('image') &&
+          item.media.url.startsWith('https://')
+        "
         key="collapse"
       >
         <nav class="level">
@@ -71,9 +115,13 @@ const props = defineProps({
         </nav>
       </div>
     </transition>
-  </div>  
+  </div>
   <transition name="fade" mode="out-in">
-    <div class="panel-block" v-if="!isCollapsed && item.hasScore" key="collapse">
+    <div
+      class="panel-block"
+      v-if="!isCollapsed && item.hasScore"
+      key="collapse"
+    >
       <div class="level">
         <div class="level-left">
           <div class="level-item">
@@ -223,7 +271,11 @@ const props = defineProps({
   <transition name="fade" mode="out-in">
     <div
       class="panel-block"
-      v-if="!isCollapsed && item.media.location !== null && item.media.location.hasAddress"
+      v-if="
+        !isCollapsed &&
+        item.media.location !== null &&
+        item.media.location.hasAddress
+      "
       key="collapse"
     >
       <div class="level">
@@ -272,7 +324,9 @@ const props = defineProps({
     <div
       class="panel-block"
       v-if="
-        !isCollapsed && item.media.description !== null && item.media.description.length > 0
+        !isCollapsed &&
+        item.media.description !== null &&
+        item.media.description.length > 0
       "
       key="collapse"
     >
@@ -280,7 +334,10 @@ const props = defineProps({
         <span class="is-size-6 is-uppercase has-text-weight-bold has-text-grey"
           >Description</span
         >
-        <p class="is-size-6 has-text-weight-bold" v-text="item.media.description"></p>
+        <p
+          class="is-size-6 has-text-weight-bold"
+          v-text="item.media.description"
+        ></p>
       </div>
     </div>
   </transition>
@@ -316,6 +373,23 @@ const props = defineProps({
       overflow: hidden;
       box-sizing: content-box;
       text-align: center;
+    }
+
+    > .level-left > .level-item > button.toggle {
+      width: fit-content !important;
+      height: fit-content !important;
+      padding: 8px !important;
+      box-shadow: none !important;
+      line-height: 1.5rem !important;
+      background: transparent !important;
+      
+      > span.icon {
+        margin: 0 !important;
+        width: 1.5rem !important;
+        height: 1.5rem !important;
+        font-size: 1.5rem !important;
+        line-height: 1.5rem !important;
+      }
     }
 
     > .level-right {
