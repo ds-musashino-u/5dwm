015af5a77c40d0a202204d278bb0e07804525827
Milch <kawatan@gmail.com>
2022-08-01 17:02:40 +0900

    Update __init__.py

--- api/medium/__init__.py
+++ api/medium/__init__.py
@@ -51,6 +51,46 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             finally:
                 session.close()
 
+        elif req.method == 'PUT':
+            if 'X-Authorization' in req.headers and req.headers['X-Authorization'].startswith('Bearer '):
+                if not verify(req.headers['X-Authorization'].split(' ')[1], os.environ['AUTH0_JWKS_URL'], os.environ['AUTH0_AUDIENCE'], os.environ['AUTH0_ISSUER'], [os.environ['AUTH0_ALGORITHM']]):
+                    return func.HttpResponse(status_code=401, mimetype='', charset='')
+
+            if req.headers.get('Content-Type') == 'application/json':
+                data = req.get_json()
+                url = data.get('url')
+
+                try:
+                    media = session.query(Media).filter(Media.id == id).one()
+                    
+                    if url is not None:
+                        media.url = url
+
+                    session.commit()
+
+                    return func.HttpResponse(json.dumps({
+                        'id': media.id,
+                        'url': media.url,
+                        'type': media.type,
+                        'categories': media.categories,
+                        'address': media.address,
+                        'description': media.description,
+                        'username': media.username,
+                        'location': {'type': 'Point', 'coordinates': [media.longitude, media.latitude]},
+                        'created_at': media.created_at.strftime('%Y-%m-%dT%H:%M:%SZ')
+                    }), status_code=200, mimetype='application/json', charset='utf-8')
+
+                except Exception as e:
+                    session.rollback()
+
+                    raise e
+
+                finally:
+                    session.close()
+
+            else:
+                return func.HttpResponse(status_code=400, mimetype='', charset='')
+
         else:
             try:
                 media = session.query(Media).filter(
