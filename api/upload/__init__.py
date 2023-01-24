347fe0f4b16c1d933df7eabd2f7f77c4625a2733
Masaaki Kawata <kawatan@gmail.com>
2023-01-24 20:35:59 +0900

    Update upload api

--- api/upload/__init__.py
+++ api/upload/__init__.py
@@ -3,36 +3,30 @@ import re
 import json
 import logging
 import os
+from io import BytesIO
 from datetime import datetime, timezone
 from uuid import uuid4
 from base64 import b64decode
 from urllib.request import urlopen, Request
+from PIL import Image
+from shared.auth import verify
+from shared.imaging import resize_image
 
 import azure.functions as func
 from azure.storage.blob import BlobServiceClient, ContentSettings
 from azure.cosmos.cosmos_client import CosmosClient
 
 
+UPLOAD_MAX_FILESIZE = os.environ.get('UPLOAD_MAX_FILESIZE', 5000000)
+
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
-        if 'Authorization' in req.headers:
-            '''
-            jwt = req.headers['Authorization'].split(' ')[1].split('.') if req.headers['Authorization'].startswith('Bearer ') else req.headers['Authorization'].split('.')
+        if req.headers['X-Authorization'].startswith('Bearer ') and not verify(req.headers['X-Authorization'].split(' ')[1], os.environ['AUTH0_JWKS_URL'], os.environ['AUTH0_AUDIENCE'], os.environ['AUTH0_ISSUER'], [os.environ['AUTH0_ALGORITHM']]):
+            return func.HttpResponse(status_code=401, mimetype='', charset='')
 
-            if json.loads(b64decode(jwt[0] + '=' * (-len(jwt[0]) % 4)))['typ'] == 'JWT' and json.loads(b64decode(jwt[1] + '=' * (-len(jwt[1]) % 4)))['iss'] == 'https://':
-                try:
-                    response = urlopen(Request(
-                        f'https://',
-                        headers={'Content-Type': 'application/json'},
-                        data=json.dumps({'idToken': req.headers['Authorization']}).encode('utf-8')))
-
-                    if response.getcode() != 200:
-                        raise Exception
-
-                except Exception:
-                    return func.HttpResponse(status_code=401, mimetype='', charset='')
-            '''
-            pass
+        if int(req.headers['Content-Length']) >= UPLOAD_MAX_FILESIZE:
+            return func.HttpResponse(status_code=413, mimetype='', charset='')
         
         if req.headers['Content-Type'] == 'application/json':
             data = req.get_json()            
@@ -41,19 +35,36 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             if match:
                 mime_type, encoding, data = match.groups()
             
-                if mime_type in ['image/apng', 'image/gif', 'image/png', 'image/jpeg', 'image/webp'] and encoding == 'base64':
+                if encoding == 'base64':
                     container_name = '$web'
                     id = str(uuid4())
-                    path = f'images/{id}'
+                    path = f'media/{id}'
+                    decoded_data = b64decode(data)
                     
                     blob_service_client = BlobServiceClient.from_connection_string(os.environ['AZURE_STORAGE_CONNECTION_STRING'])
                     container_client = blob_service_client.get_container_client(container_name)
-                
+
+                    
+                    if mime_type in ['image/apng', 'image/gif', 'image/png', 'image/jpeg', 'image/webp']:
+                        thumbnail_path = f'media/thumbnails/{id}'
+
+                        thumbnail_image = resize_image(Image.open(BytesIO(decoded_data)), 8).convert('RGB')
+                        thumbnail_bytes = BytesIO()
+                        thumbnail_image.save(thumbnail_bytes, format='PNG')
+                        
+                        blob_client = container_client.get_blob_client(thumbnail_path)
+                        blob_client.upload_blob(thumbnail_bytes.getvalue(), blob_type="BlockBlob", content_settings=ContentSettings(content_type=mime_type))
+                    else:
+                        thumbnail_path = None
+
                     blob_client = container_client.get_blob_client(path)
-                    blob_client.upload_blob(b64decode(data), blob_type="BlockBlob", content_settings=ContentSettings(content_type=mime_type))
+                    blob_client.upload_blob(decoded_data, blob_type="BlockBlob", content_settings=ContentSettings(content_type=mime_type))
                     
                     item = {'id': id, 'pk': id, 'url': f'https://5dwm.blob.core.windows.net/{container_name}/{path}', 'type': blob_client.get_blob_properties().content_settings.content_type, 'timestamp': datetime.fromtimestamp(time.time(), timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ') }
                     
+                    if thumbnail_path is not None:
+                        item["thumbnail_url"] = f'https://5dwm.blob.core.windows.net/{container_name}/{thumbnail_path}'
+
                     client = CosmosClient.from_connection_string(os.environ['AZURE_COSMOS_DB_CONNECTION_STRING'])
                     database = client.get_database_client('5DWM')
                     container = database.get_container_client('Uploads')
