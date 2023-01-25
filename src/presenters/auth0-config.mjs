96034103514de275cba7cc89cc9352cb64516e14
Masaaki Kawata <kawatan@gmail.com>
2023-01-25 17:25:39 +0900

    Add Auth0 audience

--- src/presenters/auth0-config.mjs
+++ src/presenters/auth0-config.mjs
@@ -1,4 +1,5 @@
 export class Auth0Config {
     static DOMAIN = "5dwm.jp.auth0.com";
     static CLIENT_ID = "rat15Zt97ZCoo4QjzHKJKyqIMWJJF3AA";
+    static AUDIENCE = "https://5dworldmap.com/api/v1/";
 }
