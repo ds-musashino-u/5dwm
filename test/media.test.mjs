71f8d15e7362d4f5b1c7ab78722f1ec52268eadc
Masaaki Kawata <kawatan@gmail.com>
2023-01-28 20:33:21 +0900

    Update media.test.mjs

--- test/media.test.mjs
+++ test/media.test.mjs
@@ -30,11 +30,11 @@ describe("media", function () {
     it("insertMedium", async function () {
         fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve({ id: 1, url: "https://5dworldmap.com/foobar.png", type: "image/png", categories: ["foo", "bar"], address: "foo", description: "foo bar baz", username: "foobar", location: { type: "Point", coordinates: [105.85271637244875, 21.028344772352863] }, created_at: "1970-01-01T09:00:00Z" }) }));
 
-        assert.equal(true, await insertMedium("https://5dworldmap.com/foobar.png", "image/png", ["foo", "bar"], "foo bar baz", "foobar", new Location(105.85271637244875, 21.028344772352863, "foo")) instanceof Media);
+        assert.equal(true, await insertMedium("dummy_token", "https://5dworldmap.com/foobar.png", "image/png", ["foo", "bar"], "foo bar baz", "foobar", new Location(105.85271637244875, 21.028344772352863, "foo")) instanceof Media);
     });
     it("deleteMedium", async function () {
         fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve({ id: 1, url: "https://5dworldmap.com/foobar.png", type: "image/png", categories: ["foo", "bar"], address: "foo", description: "foo bar baz", username: "foobar", location: { type: "Point", coordinates: [105.85271637244875, 21.028344772352863] }, created_at: "1970-01-01T09:00:00Z" }) }));
 
-        assert.equal(true, await deleteMedium(1) instanceof Media);
+        assert.equal(true, await deleteMedium("dummy_token", 1) instanceof Media);
     });
 });
