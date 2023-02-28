1a6357a7ff6e1ee3a8423888f3406a8020481838
Masaaki Kawata <kawatan@gmail.com>
2023-02-28 19:18:13 +0900

    Update Sidebar.vue

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -7,6 +7,7 @@ const props = defineProps({
   user: { type: Object, required: false, default: null },
   items: Array,
   index: { type: Number, required: false, default: 0 },
+  isRevealed: { type: Boolean, required: false, default: false }
 });
 const emit = defineEmits(["reveal", "select"]);
 const propsIndexRef = toRef(props, "index");
@@ -28,33 +29,21 @@ watchEffect(() => (selectedIndex.value = propsIndexRef.value));
         <div class="level">
           <div class="level-item">
             <transition name="fade" mode="out-in">
-              <button
-                class="button"
-                type="button"
-                v-bind:disabled="user === null"
-                @click="reveal"
-                key="reveal"
-              >
-                <span class="icon is-small">
-                  <i class="fas fa-bars"></i>
-                </span>
+              <button class="button" type="button" v-bind:disabled="user === null" @click="reveal" key="reveal">
+                <transition name="fade" mode="out-in">
+                  <span class="icon is-small" v-if="props.isRevealed" key="open">
+                    <i class="fa-solid fa-xmark"></i>
+                  </span>
+                  <span class="icon is-small" v-else key="close">
+                    <i class="fa-solid fa-bars"></i>
+                  </span>
+                </transition>
               </button>
             </transition>
           </div>
-          <div
-            class="level-item"
-            v-for="(item, index) in items"
-            v-bind:key="item"
-          >
-            <button
-              class="button"
-              type="button"
-              v-bind:class="{ 'has-text-primary': selectedIndex === index }"
-              v-bind:disabled="user === null"
-              v-bind:data-index="index"
-              v-bind:data-name="item.name"
-              @click="select"
-            >
+          <div class="level-item" v-for="(item, index) in items" v-bind:key="item">
+            <button class="button" type="button" v-bind:class="{ 'has-text-primary': selectedIndex === index }"
+              v-bind:disabled="user === null" v-bind:data-index="index" v-bind:data-name="item.name" @click="select">
               <span class="icon is-large">
                 <i v-bind:class="item.icon"></i>
               </span>
@@ -70,7 +59,7 @@ watchEffect(() => (selectedIndex.value = propsIndexRef.value));
 </template>
 
 <style lang="scss" scoped>
-  #sidebar {
-    border-right: 1px solid hsl(0deg, 0%, 93%);
-  }
+#sidebar {
+  border-right: 1px solid hsl(0deg, 0%, 93%);
+}
 </style>
