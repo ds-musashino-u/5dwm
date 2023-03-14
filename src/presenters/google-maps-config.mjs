d94b192918a5615a157824f631fd8c109ba65d87
Masaaki Kawata <kawatan@gmail.com>
2023-03-14 14:24:22 +0900

    Update Google Maps Config

--- src/presenters/google-maps-config.mjs
+++ src/presenters/google-maps-config.mjs
@@ -1,3 +1,15 @@
 export class GoogleMapsConfig {
     static API_KEY = "AIzaSyCJfuPZn56-CDI74WPsUPGrQ3bI6hm7H9c";
+    static VERSION = "quarterly";
+    static MAP_OPTIONS = {
+        center: { lat: 35.6809591, lng: 139.7673068 },
+        zoom: 4,
+        mapTypeId: "terrain",
+        zoomControl: true,
+        mapTypeControl: false,
+        scaleControl: true,
+        streetViewControl: false,
+        rotateControl: true,
+        fullscreenControl: false,
+    };
 }
