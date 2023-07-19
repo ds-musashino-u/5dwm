aca503ce994cf0fc0e0580f4f081d403bacce845
Masaaki Kawata <kawatan@gmail.com>
2023-07-19 13:08:42 +0900

    Update search API

--- api/search/__init__.py
+++ api/search/__init__.py
@@ -134,11 +134,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                     return func.HttpResponse(status_code=400, mimetype='', charset='')
 
                 if histogram is not None:
-                    query = query.join(ImageVector, Media.id == ImageVector.id)
-                    filters.append(Media.id.in_(session.query(ImageVector.id).filter(or_(*list(map(lambda data: ImageVector.feature == f'f{data[0]}', histogram))))))
+                    #query = query.join(ImageVector, Media.id == ImageVector.id)
+                    filters.append(Media.id.in_(session.query(ImageVector.id.distinct()).filter(or_(*list(map(lambda data: ImageVector.feature == f'f{data[0]}', histogram))))))
 
                     if limit is None:
-                        limit = 15 * 100
+                        limit = 100
 
                 if keywords is not None:
                     for keyword in keywords:
