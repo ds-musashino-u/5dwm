6130b7559e5bf33e1e7cfd6424d13386484565f5
Masaaki Kawata <kawatan@gmail.com>
2024-04-15 12:28:39 +0900

    Update uploader.mjs

--- src/presenters/uploader.mjs
+++ src/presenters/uploader.mjs
@@ -4,20 +4,23 @@ import { Endpoints } from "./endpoints.mjs";
  * /api/v1/upload
  * @module upload
  * @param {!string} token - Instance of Auth0
- * @param {!string} dataURL - Data URL
+ * @param {!object} blob - BLOB
  * @return {!object} - Result item
  */
-export async function upload(token, dataURL) {
+export async function upload(token, blob) {
+    const formData = new FormData();
+
+    formData.append("file", blob);
+
     const response = await fetch(
         Endpoints.UPLOAD_URL,
         {
             mode: "cors",
             method: "POST",
             headers: {
-                "X-Authorization": `Bearer ${token}`,
-                "Content-Type": "application/json",
+                "X-Authorization": `Bearer ${token}`
             },
-            body: JSON.stringify({ data: dataURL }),
+            body: formData,
         }
     );
 
