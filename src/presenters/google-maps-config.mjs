e5f9246011777328afb59402645709515a6cdda6
Masaaki Kawata <kawatan@gmail.com>
2024-04-30 23:22:56 +0900

    Update google-maps-config.mjs

--- src/presenters/google-maps-config.mjs
+++ src/presenters/google-maps-config.mjs
@@ -1,9 +1,11 @@
 export class GoogleMapsConfig {
     static API_KEY = "AIzaSyCJfuPZn56-CDI74WPsUPGrQ3bI6hm7H9c";
-    static VERSION = "quarterly";
+    static VERSION = "beta"; //"quarterly";
+    static MAP_ID = "DEMO_MAP_ID";
     static MAP_OPTIONS = {
         center: { lat: 35.6809591, lng: 139.7673068 },
         zoom: 4,
+        mapId: this.MAP_ID,
         mapTypeId: "terrain",
         zoomControl: true,
         mapTypeControl: false,
