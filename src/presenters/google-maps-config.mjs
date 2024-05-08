00bd9d3abea0b586c504814ae7b5df00819591ed
Masaaki Kawata <kawatan@gmail.com>
2024-05-08 15:29:15 +0900

    Update google-maps-config.mjs

--- src/presenters/google-maps-config.mjs
+++ src/presenters/google-maps-config.mjs
@@ -1,7 +1,7 @@
 export class GoogleMapsConfig {
     static API_KEY = "AIzaSyCJfuPZn56-CDI74WPsUPGrQ3bI6hm7H9c";
     static VERSION = "beta"; //"quarterly";
-    static MAP_ID = "DEMO_MAP_ID";
+    static MAP_ID = "e987a61599f86daf"; // "f0338ea7c342947b" Raster map
     static MAP_OPTIONS = {
         center: { lat: 35.6809591, lng: 139.7673068 },
         zoom: 4,
